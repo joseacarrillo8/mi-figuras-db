7d660c20951b736afadcaadb448820f1cee187c6
joseacarrillo8 <palmapesca@hotmail.com>
2025-05-19 19:38:52 +0200

    a

--- js/script.js
+++ js/script.js
@@ -2364,11 +2364,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-document.getElementById('toggleFilters').addEventListener('click', () => {
-  const filters = document.getElementById('filters-container');
-  filters.classList.toggle('hidden');
-});
-
 
 
 
