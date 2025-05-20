e2e668a18891efc0a0b97e7d4e2f763d0696e47f
joseacarrillo8 <palmapesca@hotmail.com>
2025-05-20 18:16:25 +0200

    firebase

---
+++ js/firebase-init.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9IwlIc-r-X3U2NqQArFZlH9d8lFp7PXo",
+  authDomain: "figuras-db.firebaseapp.com",
+  projectId: "figuras-db",
+  storageBucket: "figuras-db.appspot.com",
+  messagingSenderId: "9650763402",
+  appId: "1:9650763402:web:0f0aa2deb20701e3475f73"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+window.firebaseDocs = { doc, getDoc, setDoc };
