2592e51b847e8441487532453276686255226819
joseacarrillo8 <palmapesca@hotmail.com>
2025-05-22 11:09:46 +0200

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,3 +1,4 @@
+// js/firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js";
 
@@ -5,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB9IwlIc-r-X3U2NqQArFZlH9d8lFp7PXo",
   authDomain: "figuras-db.firebaseapp.com",
   projectId: "figuras-db",
-  storageBucket: "figuras-db.appspot.com",
+  storageBucket: "figuras-db.firebasestorage.app",
   messagingSenderId: "9650763402",
   appId: "1:9650763402:web:0f0aa2deb20701e3475f73"
 };
